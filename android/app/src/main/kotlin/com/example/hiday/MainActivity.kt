34bf6fabdf3a3cc3c3d41a9785ebe07a7bf3bdda
khanh3733 <nguyenduykhanhgaim@gmail.com>
2022-12-15 23:50:59 +0700

    upload

---
+++ android/app/src/main/kotlin/com/example/hiday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hiday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/HomeAppBar.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 189, 189, 189),
+      padding: EdgeInsets.only(
+        top: 50,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 12,
+                  left: 130,
+                  right: 5,
+                ),
+                child: Text(
+                  'Welcom, hienltt',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Image.asset(
+                  "images/Untitled.png",
+                  width: 50,
+                  height: 60,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 15),
+                child: Icon(
+                  Icons.calendar_today_outlined,
+                  size: 30,
+                ),
+              ),
+              Spacer(),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Icon(
+                  Icons.settings,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 40),
+            padding: EdgeInsets.only(bottom: 15),
+            child: Text(
+              "Tài chính",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
